2e44c97f904ee5310b3d367595830dd6028bc00b
Rosemary Walmsley <Rosemaryw@ndph.ox.ac.uk>
2019-10-30 17:31:39 +0000

    add functionality for alr and clr transformation

--- R/transform_comp.R
+++ R/transform_comp.R
@@ -5,19 +5,31 @@
 #'
 #' @param data Dataset to have the transformation applied to.
 #' @param comp_labels The labels of the compositional columns.
+#' @param transformation_type The type of transformation desired. Should be \code{"ilr", "alr"} or \code{"clr"}.
 #' @param rounded_zeroes Are zeroes rounded zeroes?
 #' @param det_limit Detection limit if zeroes are to be imputed. This must be set if \code{rounded_zeroes} is \code{TRUE} and should be the
 #' minimum measurable value in the compositional columns of data.
-#' @param component_1 First component in ilr-pivot coordinate transformation. Passed to \code{alter_order_comp_labels} unless \code{NULL}.
+#' @param comparison_component Only needed for alr transformation. Should be an element of \code{comp_labels}. Name of component that all other components will be compared to.
+#' @param component_1 Only used if First component in ilr-pivot coordinate transformation. Passed to \code{alter_order_comp_labels} unless \code{NULL}.
 #' @return \code{data} with ilr-transformed activity columns.
 #' @examples # TBA
 #' @export
-transform_comp <- function(data, comp_labels, rounded_zeroes = TRUE, det_limit = NULL, component_1 = NULL){
+transform_comp <- function(data, comp_labels, transformation_type = "ilr", rounded_zeroes = TRUE, det_limit = NULL, comparison_component = NULL, component_1 = NULL){
   if (rounded_zeroes & is.null(det_limit)){
     stop("det_limit must be set for zeroes to be imputed. It should be the minimum measurable value in the compositional
          columns of data.")
   }
-  if (!is.null(component_1)){
+  if (transformation_type == "alr"){
+    print("Alr transformed variables shouldn't be used for any applications which are sensitive to distance, such as PCA (applications which are not affine-equivariant).")
+  }
+  if (transformation_type == "alr" & is.null(comparison_component)){
+    stop("comparison_component must be set for alr transformation.")
+  }
+  if (transformation_type == "clr"){
+    print("Clr transformed variables are singular, so shouldn't be used for regression modelling.")
+  }
+
+  if (transformation_type == "ilr" & !is.null(component_1)){
     comp_labels <- alter_order_comp_labels(comp_labels, component_1)
   }
   dCompOnly <- data[, comp_labels]
@@ -34,10 +46,21 @@ transform_comp <- function(data, comp_labels, rounded_zeroes = TRUE, det_limit =
       dCompOnly <- dCompOnly[dCompOnly[activity] != 0, ]
     }
   }
+  if (transformation_type == "ilr"){
+    dTransformed <- ilr_trans(dCompOnly)
+    transf_labels <- transf_labels(comp_labels)
+    colnames(dTransformed) <- transf_labels
+  }
 
-  dTransformed <- ilr_trans(dCompOnly)
-  transf_labels <- transf_labels(comp_labels)
-  colnames(dTransformed) <- transf_labels
+  if (transformation_type == "alr"){
+    dTransformed <- alr_trans(dCompOnly, comp_labels, comparison_component)
+  }
+
+  if (transformation_type == "clr"){
+    dTransformed <- clr_trans(data)
+  }
   dOut <- data.frame(dDropped[, !colnames(dDropped) %in% colnames(dCompOnly)], dCompOnly, dTransformed)
+
+
   return(dOut)
 }
