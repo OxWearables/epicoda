43b179b4a9a0659ab3fe57836c0bc9a599e4ef50
Rosemary Walmsley <rosemary.walmsley@worc.ox.ac.uk>
2019-10-31 10:45:47 +0000

    add function to do predictions at different values.

---
+++ R/predict_comp.R
@@ -0,0 +1,17 @@
+#' Give model predictions for a particular compositions
+#'
+#'
+#' @param composition_list List of compositions
+#' @param model
+#' @param comp_labels The labels of the compositional columns.
+#' @param transformation_type
+#' @param comparison_component Only needed for alr transformation. Should be an element of \code{comp_labels}. Name of component that all other components will be compared to.
+#' @param component_1 Only used if First component in ilr-pivot coordinate transformation. Passed to \code{alter_order_comp_labels} unless \code{NULL}.
+
+#' @export
+predict_comp <- function(data, model, comp_labels, transformation_type = "ilr", comparison_component = NULL, component_1 = NULL)
+  if (class(model) != "coxph"){
+    stop("forest_plot_comp is only implemented for Cox models.")
+  }
+return(y)
+}
