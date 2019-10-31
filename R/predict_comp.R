db11df4431c8b34183cfb59e4fed847084253e63
Rosemary Walmsley <rosemary.walmsley@worc.ox.ac.uk>
2019-10-31 12:46:14 +0000

    correct bracketing

--- R/predict_comp.R
+++ R/predict_comp.R
@@ -9,7 +9,7 @@
 #' @param component_1 Only used if First component in ilr-pivot coordinate transformation. Passed to \code{alter_order_comp_labels} unless \code{NULL}.
 
 #' @export
-predict_comp <- function(data, model, comp_labels, transformation_type = "ilr", comparison_component = NULL, component_1 = NULL)
+predict_comp <- function(data, model, comp_labels, transformation_type = "ilr", comparison_component = NULL, component_1 = NULL){
   if (class(model) != "coxph"){
     stop("forest_plot_comp is only implemented for Cox models.")
   }
