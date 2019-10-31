693b69a11e7cf5e1f456db1f3dbe34b69e05365d
Rosemary Walmsley <rosemary.walmsley@worc.ox.ac.uk>
2019-10-31 10:28:07 +0000

    delete incorrectinfo

--- R/forest_plot_examples.R
+++ R/forest_plot_examples.R
@@ -10,16 +10,5 @@
 
 #' @export
 forest_plot_examples <- function(data, model, comp_labels, transformation_type = "ilr", comparison_component = NULL, component_1 = NULL){
-  if (rounded_zeroes & is.null(det_limit)){
-) {
-  if (!(comparison_component %in% comp_labels)){
-    stop("comparison_component must appear in comp_labels")
-  }
-  comp_labels_without_cc <- comp_labels[comp_labels != comparison_component]
-  y <- data[, comp_labels_without_cc]
-  for (label in comp_labels_without_cc){
-    y[, label]<- log(y[, label]/data[, comparison_component])
-  }
-  colnames(y) <- paste0("alr_",colnames(y), comparison_component)
   return(y)
 }
