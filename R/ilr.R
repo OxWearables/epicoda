85afd81dc7d0591f402811a4f2be3dca7d030d0f
Rosemary Walmsley <Rosemaryw@ndph.ox.ac.uk>
2019-11-12 18:05:19 +0000

    add ilr inversion

--- R/ilr.R
+++ R/ilr.R
@@ -1,6 +1,6 @@
 #' ilr_trans: Performs ilr transformations (pivot coordinates)
 #'
-#' Takes activity columns and returns them after ilr transformation.
+#' Takes compositional columns and returns them after ilr transformation.
 #'
 #' This doesn't use compositions::ilr to insulate it from changes in the choice of
 #' basis used. As pivot coordinates are currently standard in physical activity epidemiology this
@@ -17,4 +17,27 @@ ilr_trans <- function(data){
   dTransformed <- data.frame(mClr %*% transformation_matrix)
   colnames(dTransformed) <- paste0("ilr_", 1:(ncol(data)-1))
   return(dTransformed)
+}
+
+
+#' ilr_trans_inv: Inverts ilr transformations (pivot coordinates)
+#'
+#' Takes ilr-transformed columns and inverts the ilr transformation.
+#'
+#'
+#' @param data Dataset to have the transformation reversed
+#' @return Compositional columns
+#' @examples # TBA
+#' @export
+ilr_trans_inv <- function(data){
+  transformation_matrix <- create_transformation_matrix(ncol(data) + 1)
+  transformation_matrix_t <- t(transformation_matrix)
+  matrixA <- rbind(transformation_matrix_t, rep(1, by = ncol(transformation_matrix_t)))
+  aug_data <- cbind(data, rep(0, by = nrow(data)))
+  Ainv <- solve(matrixA)
+  data_ready <- t(as.matrix(aug_data))
+  dUntransformed <- data.frame(t(Ainv %*% data_ready))
+  colnames(dUntransformed) <- paste0("clr_comp_", 1:(ncol(dUntransformed)-1))
+  dReversed <- clr_trans_inv(dUntransformed)
+  return(dReversed)
 }
