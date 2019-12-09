0e107bc0f8038cb49132f51eb6082f0da32e4552
Rosemary Walmsley <Rosemaryw@ndph.ox.ac.uk>
2019-12-09 16:00:03 +0000

    update to make default terms = TRUE

--- R/plot_transfers.R
+++ R/plot_transfers.R
@@ -40,7 +40,7 @@ plot_transfers <- function(from_component,
                                 specified_units = NULL,
                                 rounded_zeroes = FALSE,
                                 det_limit = NULL,
-                                terms = FALSE,
+                                terms = TRUE,
                                 granularity = 10000){
 
   if (is.null(transformation_type)){
