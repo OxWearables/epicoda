328915e449c31233a30bc6c915e5276b5f95b1a9
Rosemary Walmsley <Rosemaryw@ndph.ox.ac.uk>
2020-01-07 15:04:24 +0000

    add documentation file for data

---
+++ R/documentation_of_data.R
@@ -0,0 +1,17 @@
+#' Example compositional data
+#'
+#' This is a simulated time use dataset.
+#'
+#' @name simdataplain
+#' @docType data
+#' @keywords data
+"simdataplain"
+
+#' Example compositional data with transformed variables
+#'
+#' This is a simulated time use dataset, with ilr pivot coordinates included.
+#'
+#' @name simdata
+#' @docType data
+#' @keywords data
+"simdata"
