1f7cca4a55995d90995c43a91043fda250a5592a
Rosemary Walmsley <Rosemaryw@ndph.ox.ac.uk>
2020-01-29 15:37:03 +0000

    correct the namespace

--- R/simplex_plotting.R
+++ R/simplex_plotting.R
@@ -64,11 +64,11 @@ plot_density_ternary <-
           ) +
           theme +
           ggtern::theme_showarrows() +
-          ggtern::xlab(label = "",
+          ggplot2::xlab(label = "",
                        labelarrow = paste(name1, "(%)", "\n")) +
-          ggtern::ylab(label = "",
+          ggplot2::ylab(label = "",
                        labelarrow = paste("\n", name2, "(%)")) +
-          ggtern::zlab(label = "",
+          ggplot2::zlab(label = "",
                        labelarrow = paste(name3, "(%)", "\n"))
       }
       else{
@@ -100,11 +100,11 @@ plot_density_ternary <-
         plot <-
           plot + theme +
           ggtern::theme_showarrows() +
-          ggtern::xlab(label = "",
+          ggplot2::xlab(label = "",
                        labelarrow = paste(name1, "(%)", "\n")) +
-          ggtern::ylab(label = "",
+          ggplot2::ylab(label = "",
                        labelarrow = paste("\n", name2, "(%)")) +
-          ggtern::zlab(label = "",
+          ggplot2::zlab(label = "",
                        labelarrow = paste(name3, "(%)", "\n"))
       }
 
@@ -123,11 +123,11 @@ plot_density_ternary <-
           ) +
           theme +
           ggtern::theme_showarrows() +
-          ggtern::xlab(label = "",
+          ggplot2::xlab(label = "",
                        labelarrow = paste(name1, "(%)", "\n")) +
-          ggtern::ylab(label = "",
+          ggplot2::ylab(label = "",
                        labelarrow = paste("\n", name2, "(%)")) +
-          ggtern::zlab(label = "",
+          ggplot2::zlab(label = "",
                        labelarrow = paste(name3, "(%)", "\n")) +
           ggtern::geom_crosshair_tern(
             data = mark_points,
@@ -171,11 +171,11 @@ plot_density_ternary <-
               color = mark_points[, groups]
             )
           ) + ggtern::theme_showarrows() +
-          ggtern::xlab(label = "",
+          ggplot2::xlab(label = "",
                        labelarrow = paste(name1, "(%)", "\n")) +
-          ggtern::ylab(label = "",
+          ggplot2::ylab(label = "",
                        labelarrow = paste("\n", name2, "(%)")) +
-          ggtern::zlab(label = "",
+          ggplot2::zlab(label = "",
                        labelarrow = paste(name3, "(%)", "\n"))
 
       }
