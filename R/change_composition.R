e404d07b65e392f905946397dd7b509ca766397b
R-Walmsley <rosemary.walmsley@gtc.ox.ac.uk>
2020-08-05 17:48:28 +0100

    make change_composition more flexible

--- R/change_composition.R
+++ R/change_composition.R
@@ -4,34 +4,50 @@
 #'
 #' @param composition This should be the starting composition as a data frame.
 #' @param main_part part the main change is happening to.
+#'  @param at_expense_of Parts the main change is at the expense of/ reallocated to.
 #' @param main_change Magnitude and direction of change e.g. +1, -0.5.
 #' @param comp_labels Compositional column labels.
 #'
 #' @return Composition after rebalancing in the described way.
-#' @examples
-#' old_comp <- comp_mean(simdata, comp_labels = c("partA", "partB", "partC", "partD", "partE"))
-#' new_comp <-
+#' @examples new_comp <-
 #' change_composition(
-#'  composition = old_comp,
+#'  composition = simdata,
 #'  main_part = "partB",
 #'  main_change = +0.5,
-#'  comp_labels = c("partA", "partB", "partC", "partD", "partE")
+#'  comp_labels = comp_labels
 #')
 #' @export
 change_composition <-
   function(composition,
            main_part,
+           at_expense_of = NULL,
            main_change,
            comp_labels){
     new_composition <- data.frame(matrix(nrow = 1, ncol = 0))
     new_composition[, main_part] <-
       composition[, main_part] + main_change
-    list_we_need <-
-      comp_labels[comp_labels != main_part]
-    for (part in list_we_need) {
-      new_composition[, part] <-
-        (rowSums(composition) - new_composition[, main_part])*(composition[, part] / (rowSums(composition) -
-                                                                                 composition[, main_part]))
+    if (is.null(at_expense_of)){
+      at_expense_of <- comp_labels[comp_labels != main_part]
     }
+    if (length(at_expense_of) >1){
+      for (part in at_expense_of) {
+        new_composition[, part] <-
+          (rowSums(composition[, c(at_expense_of, main_part)]) - new_composition[, main_part])*(composition[, part] / (rowSums(composition[, at_expense_of])))
+      }
+
+    }
+    else {
+      new_composition[, at_expense_of] <-
+        rowSums(composition[,c(at_expense_of, main_part)]) - new_composition[, main_part]
+    }
+    for (part in comp_labels){
+      if (part != main_part){
+        if (!(part %in% at_expense_of)){
+          new_composition[, part] <- composition[, part]
+        }
+      }
+
+    }
+
     return(new_composition)
   }
