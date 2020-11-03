109290947e364b089c127e52390189253b30c9a3
Rosemary Walmsley <rosemary.walmsley@gtc.ox.ac.uk>
2020-11-03 11:32:53 +0000

    improve documentation of comp_model

--- R/comp_model.R
+++ R/comp_model.R
@@ -4,42 +4,41 @@
 #'
 #' @param type Type of statistical model to use. It should be one of "linear", "logistic", "cox".
 #' @param outcome Column name of outcome variable in data. If \code{type} is \code{"linear"}, this should be a continuous variable.  If \code{type} is \code{"logistic"}, this should be a binary outcome.  If \code{type} is \code{"cox"}, if this is set it should be a \code{Surv} object from package \code{survival} (if it is set, the function defaults to attempt to use it, even if \code{follow_up_time} and \code{event} are set). If this is left as \code{NULL}, \code{follow_up_time} and \code{event} can be set instead.
-#' @param covariates Character vector of column names of covariates to adjust models for. As this is used as a character vector, special arguments to the standard models (like strata(variable) for a Cox model) can be used here.
+#' @param covariates Character vector of column names of covariates to adjust models for. As this is used as a character vector, special arguments to the standard models (like \code{"strata(variable)"} for a Cox model) can be used here.
 #' @param follow_up_time Only used if \code{type} is \code{"cox"} and \code{outcome} is \code{NULL}.  Follow-up time.
 #' @param event Only used if \code{type} is \code{"cox"} and \code{outcome} is \code{NULL}.Binary variable indicating whether or not an event was observed.
 #' @inheritParams transform_comp
 #'
-#' @return model
-#' @examples
-#'  comp_model(type = "linear",
-#' outcome = "linear_outcome",
+#' @return Model using a compositional exposure variable (\code{lm}, \code{glm} or \code{coxph} object as appropriate).
+#' @examples comp_model(type = "linear",
+#' outcome = "BMI",
 #' covariates = c("agegroup", "sex"),
-#' follow_up_time = NULL,
-#' event = NULL,
-#' data = simdata,
-#' comp_labels = c("partA", "partB", "partC", "partD", "partE"),
-#' rounded_zeroes = TRUE,
-#' det_limit = 0.0083)
+#' comp_labels = c("vigorous", "moderate", "light", "sedentary", "sleep"),
+#' data = simdata)
 #' @export
 comp_model <-
   function(type = NULL,
            outcome = NULL,
            covariates = NULL,
+           comp_labels,
            follow_up_time = NULL,
            event = NULL,
            data,
-           comp_labels,
            transformation_type = "ilr",
            rounded_zeroes = TRUE,
            det_limit = NULL,
            comparison_part = NULL,
            part_1 = NULL) {
+
+    # Check information specified is correct
     if ((type != "linear") && (type != "logistic") && (type != "cox")) {
       stop("type must be \"linear\", \"logistic\", or \"cox\".")
     }
     if (transformation_type == "clr"){
       stop("clr variables are singular and shouldn't be used for regression modelling.")
     }
+
+    # Transform data
     data_ready <- transform_comp(
       data = data,
       comp_labels = comp_labels,
@@ -49,12 +48,15 @@ comp_model <-
       comparison_part = comparison_part,
       part_1 = part_1
     )
+
+    # Create  strings to specify exposure variables in models
     transf_vec <-
       transf_labels(comp_labels = comp_labels, transformation_type = transformation_type, comparison_part = comparison_part,
                     part_1 = part_1)
     transf_sum <- vector_to_sum(transf_vec)
     cov_sum <- vector_to_sum(covariates)
 
+    # Perform modelling by type
     if (type == "linear") {
       if (is.null(outcome)){
         stop("outcome must be set.")
@@ -111,10 +113,9 @@ comp_model <-
           "survival_object ~", cov_sum, "+", transf_sum
         )), data = data_ready)
       }
-
     }
 
-
+    # Return model
     return(model)
 
   }
