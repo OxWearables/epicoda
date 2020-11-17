f1a634cecae8f6b99bf53591c0ce67ec9f4d3ae5
Rosemary Walmsley <rosemary.walmsley@gtc.ox.ac.uk>
2020-11-17 12:00:35 +0000

    add tests to simplex_plotting

--- R/simplex_plotting.R
+++ R/simplex_plotting.R
@@ -1,9 +1,11 @@
 #' plot_density_ternary: Plot data density on a ternary plot
 #'
-#' Plot data density on a ternary plot
+#' Plot data density on a ternary plot.
+#'
+#' This is a wrapper for \code{ggtern}.
 #'
 #' @param data Data frame containing data to be plotted.
-#' @param groups Name of variable in the data frame which identifies the groups to be plotted. This variable should be a factor variable.
+#' @param groups If plotting groups separately, name of the variable in the data frame which identifies the groups to be plotted. This variable should be a factor variable.
 #' @param parts_to_plot Names of the three variables in the data frame which are to be plotted on the ternary plot. Note they should be on the same scale (they don't need to be normalised to 1).
 #' @param n_bins Number of bins to use on density plot.
 #' @param mark_points Points should be the rows of a data frame with the elements of \code{parts_to_plot} as columns names. If a \code{groups} argument is given, it should also have a column for this (if the groups aren't relevant to the point in a certain row, this can be set as NA).
@@ -11,10 +13,10 @@
 #' @inheritParams plot_transfers
 #' @return Plot showing density of data on ternary plot.
 #' @examples
-#' simdata$combined_parts <- simdata$partA + simdata$partB + simdata$partC
+#' simdata$activity <- simdata$vigorous + simdata$moderate + simdata$light
 #'
 #' plot_density_ternary(data = simdata,
-#' parts_to_plot = c("combined_parts", "partD", "partE"),
+#' parts_to_plot = c("activity", "sedentary", "sleep"),
 #' n_bins = 10 # This argument specifies we want to use 10 bins
 #' # (i.e. 10% of data lies between each pair of contours on the plot.)
 #' )
@@ -33,6 +35,7 @@ plot_density_ternary <-
     name1 <- parts_to_plot[1]
     name2 <- parts_to_plot[2]
     name3 <- parts_to_plot[3]
+
     if (is.null(theme)) {
       theme <- ggplot2::theme(
         plot.background = ggplot2::element_rect(fill = "white"),
---
+++ tests/testthat/test-simplex_plotting.R
@@ -0,0 +1,9 @@
+simdata$activity <- simdata$vigorous + simdata$moderate + simdata$light
+
+test_that("error if try to plot too many parts", {
+  expect_error(plot_density_ternary(data = simdata,
+                                    parts_to_plot = c("activity", "sedentary", "sleep", "moderate"),
+                                    n_bins = 10 # This argument specifies we want to use 10 bins
+
+  ))
+})
