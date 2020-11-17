0fc058ba8c880434b8de16ea58f8f36e23a003f8
Rosemary Walmsley <rosemary.walmsley@gtc.ox.ac.uk>
2020-11-17 15:18:10 +0000

    update data documentation

--- R/documentation_of_data.R
+++ R/documentation_of_data.R
@@ -1,17 +1,9 @@
 #' Example compositional data
 #'
-#' This is a simulated time use dataset.
+#' This is a simulated physical behaviour dataset.
 #'
 #' @name simdata
 #' @docType data
 #' @keywords data
 "simdata"
 
-#' Example compositional data with transformed variables
-#'
-#' This is a simulated time use dataset, with ilr pivot coordinates included.
-#'
-#' @name simdata_ilr
-#' @docType data
-#' @keywords data
-"simdata_ilr"
