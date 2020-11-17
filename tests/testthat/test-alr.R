3b0c2cf8789883fbe9d00e567b6b7d26e21c110b
Rosemary Walmsley <rosemary.walmsley@gtc.ox.ac.uk>
2020-11-17 15:49:44 +0000

    update alr when added zeroes to simdata

--- tests/testthat/test-alr.R
+++ tests/testthat/test-alr.R
@@ -1,19 +1,26 @@
 comp_labels <- c("vigorous", "moderate", "light", "sedentary", "sleep")
-d <- readRDS("../test_data/alr_output.RDS")
+sd_zf <- simdata[simdata$vigorous != 0,]
 
-test_that("Output of alr transformation agrees with reference", {
-  expect_equal(alr_trans(simdata[1 ,comp_labels], comp_labels, "sleep"),d )
+for (label in comp_labels){
+  sd_zf <- sd_zf[sd_zf[, label] != 0, ]
+}
+rownames(sd_zf) <- NULL
+
+test_that("result of alr transformation unchanged from reference", {
+  expect_equal_to_reference(alr_trans(sd_zf[1 ,comp_labels], comp_labels, "sleep"), "../test_data/alr_output.RDS")
 })
+
+
 test_that("Throws error if comparison part incorrectly specified", {
-  expect_error(alr_trans(simdata[1 ,comp_labels], comp_labels, "not_a_column"))
+  expect_error(alr_trans(sd_zf[1 ,comp_labels], comp_labels, "not_a_column"))
 })
 
 test_that("Throws error if comp_labels incorrectly specified", {
-  expect_error(alr_trans(simdata[1 ,comp_labels], c("not_the_right_columns")))
+  expect_error(alr_trans(sd_zf[1 ,comp_labels], c("not_the_right_columns")))
 })
 
 test_that("Throws error if comparison_part not specified", {
-  expect_error(epicoda::alr_trans(data = simdata, comp_labels = comp_labels))
+  expect_error(epicoda::alr_trans(data = sd_zf, comp_labels = comp_labels))
 })
 
 
