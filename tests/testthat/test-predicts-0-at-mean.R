e0ec417c9b29886d6988f419f0d71ace83687c5c
Rosemary Walmsley <rosemary.walmsley@gtc.ox.ac.uk>
2021-03-27 08:26:44 +0000

    enhance tests at compositional mean

--- tests/testthat/test-predicts-0-at-mean.R
+++ tests/testthat/test-predicts-0-at-mean.R
@@ -7,6 +7,22 @@ m <- comp_model(
   comp_labels = comp_labels,
   rounded_zeroes = TRUE
 )
+m2 <- comp_model(
+  type = "logistic",
+  data = simdata,
+  outcome = "disease",
+  covariates = c("agegroup", "sex"),
+  comp_labels = comp_labels,
+  rounded_zeroes = TRUE
+)
+m3 <- comp_model(
+  type = "cox",
+  data = simdata,
+  outcome = survival::Surv(simdata$follow_up_time, simdata$event),
+  covariates = c("agegroup", "sex"),
+  comp_labels = comp_labels,
+  rounded_zeroes = TRUE
+)
 nd <- comp_mean(
   data = simdata,
   rounded_zeroes = TRUE,
@@ -17,4 +33,41 @@ test_that("prediction at the comp mean is 0", {
                predict_fit_and_ci(
                  model = m,
                  new_data = nd, comp_labels = comp_labels,terms = TRUE)[1, "fit"])
+})
+
+test_that("prediction at the comp mean is 0", {
+  expect_equal(0,
+               as.numeric(predict_fit_and_ci(
+                 model = m,
+                 new_data = nd, comp_labels = comp_labels,terms = TRUE)[1, "upper_CI"]))
+})
+
+
+test_that("prediction at the comp mean is 1 - logistic", {
+  expect_equal(1,
+               predict_fit_and_ci(
+                 model = m2,
+                 new_data = nd, comp_labels = comp_labels,terms = TRUE)[1, "fit"])
+})
+
+
+test_that("prediction at the comp mean is 1 - logistic", {
+  expect_equal(1,
+               as.numeric(predict_fit_and_ci(
+                 model = m2,
+                 new_data = nd, comp_labels = comp_labels,terms = TRUE)[1, "lower_CI"]))
+})
+
+test_that("prediction at the comp mean is 1 - cox", {
+  expect_equal(1,
+               predict_fit_and_ci(
+                 model = m3,
+                 new_data = nd, comp_labels = comp_labels,terms = TRUE)[1, "fit"])
+})
+
+test_that("prediction at the comp mean is 1 - cox", {
+  expect_equal(1,
+               as.numeric(predict_fit_and_ci(
+                 model = m3,
+                 new_data = nd, comp_labels = comp_labels,terms = TRUE)[1, "upper_CI"]))
 })
