c4a609b1752f0b5b2bc111f307354a8c91fcfea7
Rosemary Walmsley <rosemary.walmsley@gtc.ox.ac.uk>
2021-05-21 18:41:49 +0100

    refactor to take repeated functionality into extract-model-properties functions

---
+++ tests/testthat/test-extract_model_properties.R
@@ -0,0 +1,72 @@
+lm_outcome <- comp_model(
+  type = "linear",
+  outcome = "BMI",
+  covariates = c("agegroup", "sex"),
+  data = simdata,
+  comp_labels = c("vigorous", "moderate", "light", "sedentary", "sleep")
+)
+
+log_outcome <- comp_model(
+  type = "logistic",
+  outcome = "disease",
+  covariates = c("agegroup", "sex"),
+  data = simdata,
+  comp_labels = c("vigorous", "moderate", "light", "sedentary", "sleep")
+)
+cox_outcome <- comp_model(
+  type = "cox",
+  event = "event",
+  follow_up_time = "follow_up_time",
+  covariates = c("agegroup", "sex"),
+  data = simdata,
+  comp_labels = c("vigorous", "moderate", "light", "sedentary", "sleep")
+)
+
+comp_labels <- c("vigorous", "moderate", "light", "sedentary", "sleep")
+tl <- transf_labels(comp_labels = comp_labels, transformation_type = "ilr")
+
+sd <- transform_comp(simdata, comp_labels = comp_labels)
+d <- get_dataset_from_model(model = lm_outcome, comp_labels = comp_labels,
+                            transf_labels = tl, type = "linear")
+
+test_that("dataset is same as if generate by hand", {
+  expect_gt(length(colnames(d)), 0)
+  expect_equal(d, sd[, colnames(d)] )
+})
+d <- get_dataset_from_model(model = log_outcome, comp_labels = comp_labels,
+                            transf_labels = tl, type = "logistic")
+
+test_that("dataset is same as if generate by hand", {
+  expect_gt(length(colnames(d)), 0)
+  expect_equal(d, sd[, colnames(d)] )
+})
+
+d <- get_dataset_from_model(model = cox_outcome, comp_labels = comp_labels,
+                            transf_labels = tl, type = "cox")
+
+test_that("dataset is same as if generate by hand", {
+  expect_gt(length(colnames(d)), 0)
+  expect_equal(d, sd[, colnames(d)] )
+})
+
+cm <- comp_mean(simdata, comp_labels = comp_labels)
+d <- get_cm_from_model(model = lm_outcome, comp_labels = comp_labels,
+                            transf_labels = tl)$cm
+
+test_that("comp mean is same as if generate by hand", {
+  expect_equal(d, cm)
+})
+
+d <- get_cm_from_model(model = log_outcome, comp_labels = comp_labels,
+                       transf_labels = tl)$cm
+
+test_that("comp mean is same as if generate by hand", {
+  expect_equal(d, cm)
+})
+
+d <- get_cm_from_model(model = cox_outcome, comp_labels = comp_labels,
+                       transf_labels = tl)$cm
+
+test_that("comp mean is same as if generate by hand", {
+  expect_equal(d, cm)
+})
