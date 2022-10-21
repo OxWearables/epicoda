6166b5bd7a15b9fbdbb70ed8ef8cf57f72d16070
Rosemary Walmsley <rosemary.walmsley@gtc.ox.ac.uk>
2022-10-21 17:21:40 +0100

    correct rep and reorganise

--- R/prepare_for_plotting.R
+++ R/prepare_for_plotting.R
@@ -56,6 +56,8 @@ make_new_data <- function(from_part,
                           lower_quantile = 0.05,
                           upper_quantile = 0.95,
                           granularity = 10000) {
+
+  # PREP ====================================================
   dataset <- normalise_comp(dataset, comp_labels)
   fixed_values <- normalise_comp(fixed_values, comp_labels)
   new_data <- data.frame()[1:granularity,]
@@ -64,6 +66,12 @@ make_new_data <- function(from_part,
   comp_sum <- as.numeric(process_units(units, specified_units)[2])
   units <- process_units(units, specified_units)[1]
 
+  # PROCESS NON COMPOSITIONAL VARIABLES  =====================
+  for (label in colnames(dataset)[!(colnames(dataset) %in% comp_labels)]){
+    new_data[, label] <- fixed_values[1, label]
+  }
+
+  # PROCESS COMPOSITIONAL VARIABLES ==========================
   # Vary the to part and the from part from the upper quantile to lower quantile
   vpi_tp <- vary_part_of_interest(dataset[, to_part],
                                   lower_quantile,
@@ -81,48 +89,35 @@ make_new_data <- function(from_part,
   max_tp <- max(vpi_tp)
 
   # Use the values from the narrower range
-  if ((max_tp - min_tp) >= (max_fp - min_fp)){
-    for (label in comp_labels) {
-      if (label == from_part) {
-        this_col <- data.frame(vpi_fp)
-        new_data[label] <- this_col
-        }
-      if (label != from_part){
-        this_col <- data.frame(rep(fixed_values[1, label], by = granularity))
-        new_data[label] <- this_col
-      }
-    }
-    for (label in colnames(dataset)[!(colnames(dataset) %in% comp_labels)]){
-      this_col <- data.frame(rep(fixed_values[1, label], by = granularity))
-      new_data[label] <- this_col
-    }
-    tt <- rep(1, by = granularity)
+  if ((max_tp - min_tp) >= (max_fp - min_fp)){ # if from_part is narrower assign this first
+    tt <- 1
     for (label in comp_labels[comp_labels != to_part]){
+      if (label == from_part) {
+        new_data[, label] <- vpi_fp
+      }
+      if (label != from_part){
+        new_data[, label] <- fixed_values[1, label]
+      }
       tt <- tt - new_data[, label]
     }
     new_data[, to_part] <- tt
   }
-  else {
-    for (label in comp_labels) {
+  else {# if to_part is narrower assign this first
+    tf <- 1
+    for (label in comp_labels[comp_labels != from_part]){
       if (label == to_part) {
-        this_col <- data.frame(vpi_tp)
-        new_data[label] <- this_col
+        new_data[, label] <- vpi_tp
       }
       if (label != to_part){
-        this_col <- data.frame(rep(fixed_values[1, label], by = granularity))
-        new_data[label] <- this_col
+        new_data[, label] <- fixed_values[1, label]
       }
-    }
-    for (label in colnames(dataset)[!(colnames(dataset) %in% comp_labels)]){
-      this_col <- data.frame(rep(fixed_values[1, label], by = granularity))
-      new_data[label] <- this_col
-    }
-    tf <- rep(1, by = granularity)
-    for (label in comp_labels[comp_labels != from_part]){
       tf <- tf - new_data[, label]
     }
     new_data[, from_part] <- tf
   }
+
+  # REORDER AND PREPARE FOR OUTPUT======================
+  new_data <- new_data[, c(comp_labels, colnames(dataset)[(colnames(dataset) %in% colnames(fixed_values)) & !(colnames(dataset) %in% comp_labels)])] # ensure same order as previously so backwards compatible
 
   # Rescale composition to be on the relevant scale
   new_data <- rescale_comp(new_data, comp_sum = comp_sum, comp_labels = comp_labels)
